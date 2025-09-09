6372460389fc7cf5619542b8e669367c2b3bb0d7
leo.perrin <perrin.leo@gmail.com>
2025-09-09 11:39:37 +0200

    changed install backend

---
+++ implementations/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup( 
+    name = "wsPRF",
+    version = "2.0",
+    description = "SAGE/Python functions useful for studying S-boxes and Boolean functions such as computing the DDT, the Walsh spectrum, investigating their (CCZ/affine/EA equivalence classes...",
+    packages = find_packages(),
+)
+
+
