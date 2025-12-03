7127c4de8bf886667f6f077a36fe320f44f06806
leo.perrin <perrin.leo@gmail.com>
2025-12-03 14:52:05 +0100

    cleaned up EALPN, switched to googl-style docstring

--- implementations/wsPRF/Ealpn.py
+++ implementations/wsPRF/Ealpn.py
@@ -14,37 +14,40 @@ def greater_than(x, k):
     
 
 class EALPN:
-    """An implementation of the EA-LPN PRF. It uses the following
-    parameters:
+    """An implementation of the EA-LPN PRF, as specified in
 
-    N: code-words of the underlying code are of size 5N
+    Elette Boyle, Geoffroy Couteau, Niv Gilboa, Yuval Ishai, Lisa Kohl, Nicolas Resch, and Peter Scholl. Correlated pseudorandomness from expand-accumulate codes. In Yevgeniy Dodis and Thomas Shrimpton, editors, CRYPTO 2022, Part II, volume 13508 of LNCS, pages 603–633. Springer, Cham, August 2022.
     
-    t: is the number of cells for the noise; in each cell, the noise
-    is all zero before a threshold, all 1 after
-
-    l: the number of cells in the input, it has to be a divisor of t
-    """    
-    def __init__(
-            self,
-            m, # main size (5N/t in slides)
-            l, # number of rows in the key
-            u, # length of each key row
-    ):
-        # initializing the variables
+    """
+    
+    def __init__(self, m, l, u,):
+        """Initializes an instance of EALPN by specifying all its parameters.
+        
+        Args:
+            N (int): code-words of the underlying code are of size 5N.
+            t (int) : is the number of cells for the noise; in each cell, the noise is all zero before a threshold, all 1 after.
+            l (int): the number of cells in the input, it has to be a divisor of t.
+        """
         self.m = m
         self.l = l
         self.u = u
 
         
     def __call__(self, k, jx):
-        """Both the key `k` and the input `jx` are two-dimensional arrays:
-
+        """Evaluates EALP on the given secret key `k` and input `jx`. Both the key `k` and the input `jx` must be two-dimensional arrays:
         - `k` must contain l rows, each with u elements in {1, ..., m}
         - `jx` is contains l rows, each with two elements:
             - `j` in {0, ..., u-1} (index in key row)
             - `x` in {1, ..., m} (value to compare)
+
+        Args:
+            k (list[list]): the secret key
+            jx (list[list]): the public input
+        
+        Returns:
+            int: An integer equal to 0 or 1.
+
         """
-        # computing the function
         result = 0
         for i in range(0, self.l):
             j, x = jx[i]
