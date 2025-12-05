a0514476547d81642c619a3303340f64fd081418
leo.perrin <perrin.leo@gmail.com>
2025-12-05 15:13:07 +0100

    updated VDLPN

--- implementations/wsPRF/Vdlpn.py
+++ implementations/wsPRF/Vdlpn.py
@@ -7,7 +7,7 @@ from .Utils import *
 
 
 def triangular(_x):
-    """The triangular function used in VDLPN. It is a Boolean function returning x_1(1 + x_2(1 + ... )).
+    """The triangular function used in VDLPN. It is a Boolean function returning x_1(1 + x_2(1 + ... )). While it is not used directly in the evaluation, it could be used in an equivalent representation.
 
     Args:
         _x (int): an integer whose binary representation is \\sum_i x_i 2^i
@@ -34,14 +34,25 @@ class VDLPN:
         """Initializes the parameters of this VDLPN instance.
 
         Args:
-            d (int):
-        # !CONTINUE! finish docstrings VDLPN 
+            d (int): the degree of the triangular function
+            w (int): the number of calls to the triangular function
+        
         """
         self.d = d
         self.w = w
         
         
     def __call__(self, k, x):
+        """Evaluates VDLPN on the given secret key `k` and public input `x`. The evaluation is not made using the sum of triangular functions (as shown at the bottom of page 27 of our paper) but rather using Equation (4) of said paper (see also below in the description of what is returned).
+
+        Args:
+            k (list): a list of integers in [0,1] corresponding to the secret key
+            x (list): a list of integers in [0,1] corresponding to the public input
+
+        Returns:
+            int: an integer in [0,1] equal to F_k(x) = \\sum_{i=1}^D \\sum_{j=1}^w \\prod_{\ell=1}^i (x_{i,j,l} + k_{i,j,l})
+        
+        """
         assert len(x) == self.w * self.d**2
         assert len(k) == self.w * self.d**2
         y = 0
